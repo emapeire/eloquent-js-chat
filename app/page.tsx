12688c28fe0ff851a8b30490e82de3e4146ecb73
Emanuel Peire <emapeire@outlook.com>
2023-11-22 13:54:45 -0300

    Fix formatting in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Header from "@/app/components/header";
-import ChatSection from "./components/chat-section";
+import Header from '@/app/components/header'
+import ChatSection from './components/chat-section'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-10 p-24 background-gradient">
+    <main className='flex min-h-screen flex-col items-center gap-10 p-20 background-gradient'>
       <Header />
       <ChatSection />
     </main>
-  );
+  )
 }
