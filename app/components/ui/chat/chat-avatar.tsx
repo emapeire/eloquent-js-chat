fd3fc465033294bd814cc8bc51e81af161cc4ead
Emanuel Peire <emapeire@outlook.com>
2023-11-22 13:28:10 -0300

    Refactor chat avatar component

--- app/components/ui/chat/chat-avatar.tsx
+++ app/components/ui/chat/chat-avatar.tsx
@@ -1,25 +1,25 @@
-import { User2 } from "lucide-react";
-import Image from "next/image";
+import { User2 } from 'lucide-react'
+import Image from 'next/image'
 
 export default function ChatAvatar({ role }: { role: string }) {
-  if (role === "user") {
+  if (role === 'user') {
     return (
-      <div className="flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border bg-background shadow">
-        <User2 className="h-4 w-4" />
+      <div className='flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border bg-background shadow'>
+        <User2 className='h-4 w-4' />
       </div>
-    );
+    )
   }
 
   return (
-    <div className="flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border bg-black text-white shadow">
+    <div className='flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border bg-black text-white shadow'>
       <Image
-        className="rounded-md"
-        src="/llama.png"
-        alt="Llama Logo"
+        className='rounded-md'
+        src='/js-logo.png'
+        alt='Eloquent Logo'
         width={24}
         height={24}
         priority
       />
     </div>
-  );
+  )
 }
