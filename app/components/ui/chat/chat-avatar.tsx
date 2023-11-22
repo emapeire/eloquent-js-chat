0a57f3f2f03b090f6faf2dd7d9adcb4747eb154d
Emanuel Peire <emapeire@outlook.com>
2023-11-22 13:33:08 -0300

    Update ChatAvatar component styles

--- app/components/ui/chat/chat-avatar.tsx
+++ app/components/ui/chat/chat-avatar.tsx
@@ -11,13 +11,13 @@ export default function ChatAvatar({ role }: { role: string }) {
   }
 
   return (
-    <div className='flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border bg-black text-white shadow'>
+    <div className='flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border bg-white text-white shadow'>
       <Image
-        className='rounded-md'
+        className='rounded-md border shadow'
         src='/js-logo.png'
         alt='Eloquent Logo'
-        width={24}
-        height={24}
+        width={32}
+        height={32}
         priority
       />
     </div>
