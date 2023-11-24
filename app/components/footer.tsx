7dfa0819d3c5179ce37ca33cc6aeeb64c4b8dd53
Emanuel Peire <emapeire@outlook.com>
2023-11-24 15:54:50 -0300

    Update footer and header styles

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className='fixed bottom-0 left-0 flex w-full items-center font-mono text-sm justify-center pb-8'>
-      <p className='flex items-center justify-center gap-2 shadow-md border rounded-md bg-white dark:bg-black border-neutral-200 dark:border-neutral-800 px-4 py-2.5 text-black dark:text-white'>
+      <p className='flex items-center justify-center gap-2 shadow-md border rounded-md bg-white dark:bg-black border-neutral-200 dark:border-neutral-800 px-4 py-2.5 text-black dark:text-white cursor-default'>
         Built by
         <a
           href='https://github.com/emapeire'
