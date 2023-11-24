6d28f960288961819e4e39a0949dd5a880a754aa
Emanuel Peire <emapeire@outlook.com>
2023-11-24 16:17:50 -0300

    Refactor chat avatar component styles

--- app/components/ui/chat/chat-avatar.tsx
+++ app/components/ui/chat/chat-avatar.tsx
@@ -11,9 +11,9 @@ export default function ChatAvatar({ role }: { role: string }) {
   }
 
   return (
-    <div className='flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border bg-white text-white shadow'>
+    <div className='flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md shadow'>
       <Image
-        className='rounded-md border shadow'
+        className='rounded-md border'
         src='/js-logo.png'
         alt='Eloquent Logo'
         width={32}
