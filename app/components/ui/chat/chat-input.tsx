9031db94433ef8058ce1eb4a0fde1057ba486682
Emanuel Peire <emapeire@outlook.com>
2023-11-24 15:32:22 -0300

    Refactor chat input component

--- app/components/ui/chat/chat-input.tsx
+++ app/components/ui/chat/chat-input.tsx
@@ -1,29 +1,29 @@
-import { Button } from "../button";
-import { Input } from "../input";
-import { ChatHandler } from "./chat.interface";
+import { Button } from '../button'
+import { Input } from '../input'
+import { ChatHandler } from './chat.interface'
 
 export default function ChatInput(
   props: Pick<
     ChatHandler,
-    "isLoading" | "handleSubmit" | "handleInputChange" | "input"
-  >,
+    'isLoading' | 'handleSubmit' | 'handleInputChange' | 'input'
+  >
 ) {
   return (
     <form
       onSubmit={props.handleSubmit}
-      className="flex w-full items-start justify-between gap-4 rounded-xl bg-white p-4 shadow-xl"
+      className='flex w-full items-start justify-between gap-4 border border-neutral-200 dark:border-neutral-800 rounded-md bg-white dark:bg-black p-4 shadow-md'
     >
       <Input
         autoFocus
-        name="message"
-        placeholder="Type a message"
-        className="flex-1"
+        name='message'
+        placeholder='Type a message'
+        className='flex-1'
         value={props.input}
         onChange={props.handleInputChange}
       />
-      <Button type="submit" disabled={props.isLoading}>
+      <Button type='submit' disabled={props.isLoading}>
         Send message
       </Button>
     </form>
-  );
+  )
 }
