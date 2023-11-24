b14a60ba7eeb85d3d75d6f75a7695533da147d77
Emanuel Peire <emapeire@outlook.com>
2023-11-24 12:07:36 -0300

    Add new files and update dependencies

--- next.config.js
+++ next.config.js
@@ -1,11 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["llamaindex"],
-    outputFileTracingIncludes: {
-      "/*": ["./cache/**/*"],
-    },
+  webpack: (config) => {
+    // See https://webpack.js.org/configuration/resolve/#resolvealias
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      sharp$: false,
+      'onnxruntime-node$': false,
+      mongodb$: false
+    }
+    return config
   },
-};
+  experimental: {
+    serverComponentsExternalPackages: ['llamaindex'],
+    outputFileTracingIncludes: {
+      '/*': ['./cache/**/*']
+    }
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
