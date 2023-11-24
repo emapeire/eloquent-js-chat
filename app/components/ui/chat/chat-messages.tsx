d684080de6ede46ec4abf02439fc2d6446a9a2ef
Emanuel Peire <emapeire@outlook.com>
2023-11-24 16:06:20 -0300

    Refactor chat messages component

--- app/components/ui/chat/chat-messages.tsx
+++ app/components/ui/chat/chat-messages.tsx
@@ -1,44 +1,44 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef } from 'react'
 
-import ChatActions from "./chat-actions";
-import ChatMessage from "./chat-message";
-import { ChatHandler } from "./chat.interface";
+import ChatActions from './chat-actions'
+import ChatMessage from './chat-message'
+import { ChatHandler } from './chat.interface'
 
 export default function ChatMessages(
-  props: Pick<ChatHandler, "messages" | "isLoading" | "reload" | "stop">,
+  props: Pick<ChatHandler, 'messages' | 'isLoading' | 'reload' | 'stop'>
 ) {
-  const scrollableChatContainerRef = useRef<HTMLDivElement>(null);
-  const messageLength = props.messages.length;
-  const lastMessage = props.messages[messageLength - 1];
+  const scrollableChatContainerRef = useRef<HTMLDivElement>(null)
+  const messageLength = props.messages.length
+  const lastMessage = props.messages[messageLength - 1]
 
   const scrollToBottom = () => {
     if (scrollableChatContainerRef.current) {
       scrollableChatContainerRef.current.scrollTop =
-        scrollableChatContainerRef.current.scrollHeight;
+        scrollableChatContainerRef.current.scrollHeight
     }
-  };
+  }
 
   const isLastMessageFromAssistant =
-    messageLength > 0 && lastMessage?.role !== "user";
+    messageLength > 0 && lastMessage?.role !== 'user'
   const showReload =
-    props.reload && !props.isLoading && isLastMessageFromAssistant;
-  const showStop = props.stop && props.isLoading;
+    props.reload && !props.isLoading && isLastMessageFromAssistant
+  const showStop = props.stop && props.isLoading
 
   useEffect(() => {
-    scrollToBottom();
-  }, [messageLength, lastMessage]);
+    scrollToBottom()
+  }, [messageLength, lastMessage])
 
   return (
-    <div className="w-full rounded-xl bg-white p-4 shadow-xl pb-0">
+    <div className='w-full border border-neutral-200 dark:border-neutral-800 rounded-md bg-white dark:bg-black p-4 shadow-md pb-0'>
       <div
-        className="flex h-[50vh] flex-col gap-5 divide-y overflow-y-auto pb-4"
+        className='flex h-[50vh] flex-col gap-5 divide-y overflow-y-auto pb-4'
         ref={scrollableChatContainerRef}
       >
         {props.messages.map((m) => (
           <ChatMessage key={m.id} {...m} />
         ))}
       </div>
-      <div className="flex justify-end py-4">
+      <div className='flex justify-end py-4'>
         <ChatActions
           reload={props.reload}
           stop={props.stop}
@@ -47,5 +47,5 @@ export default function ChatMessages(
         />
       </div>
     </div>
-  );
+  )
 }
